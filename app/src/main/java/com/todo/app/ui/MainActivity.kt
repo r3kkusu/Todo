01686fab368f75aa808496aad5ca45a4c198eabe
rexpinili <rpinili@safehealth.me>
2022-07-20 10:14:25 +0800

    Navigation bar

--- app/src/main/java/com/todo/app/ui/MainActivity.kt
+++ app/src/main/java/com/todo/app/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.todo.app.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.todo.app.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
